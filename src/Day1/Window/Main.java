66400ec7062338cd42f33232e54183ecc0047457
doncardz <carloventocilla11@gmail.com>
2026-05-24 18:43:40 +0800

    added Window

--- src/Day1/Window/Main.java
+++ src/Day1/Window/Main.java
@@ -1,5 +1,12 @@
 package Day1.Window;
 
-public class Main {
+import javax.swing.JFrame;
 
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Practice");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
 }
