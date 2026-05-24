bfac6a775170bf38cf00c832a8e25193b493cfc2
doncardz <carloventocilla11@gmail.com>
2026-05-24 18:49:00 +0800

    added JLabel, fixed Window size

--- src/Day1/JLabel/Main.java
+++ src/Day1/JLabel/Main.java
@@ -1,13 +1,19 @@
-package Day1.Window;
+package Day1.JLabel;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Practice");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null);
         frame.setSize(450,450);
+        frame.setLayout(new BorderLayout());
+
+        JLabel label = new JLabel("Hello World");
+        frame.add(label, BorderLayout.CENTER);
+
         frame.setVisible(true);
     }
 }
